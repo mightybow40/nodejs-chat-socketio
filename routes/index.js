613f116ea2dafa2a8f4bb5729a1493c4c57b1f02
Eugene Yuskiv <jeck1999@mail.ru>
2018-09-06 01:32:52 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,8 @@ app.post('/logout', main.post);
 app.get('/login', loginPage.get);
 app.post('/auth', loginPage.post);
 app.get('/chat', require('../public/javascripts/checkUser') ,require('./chat').get);
-app.get('/user', require('./user').get);
 app.get('/singin', require('./registration').get);
 app.post('/singin', require('./registration').post);
 app.post('/send', require('./chat').post);
-app.get('/user', require('./user').get);
-
-
-
-
-
-
-
+app.get('/user/:name?', require('./user').get);
 };
